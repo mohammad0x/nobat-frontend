23b05e15b0beee5508301aacf650fca93046a7dd
Erfank <kahania726@gmail.com>
2023-06-19 16:52:39 +0330

    header started

---
+++ app/components/header/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className='bg-green-700 h-80 text-center'>
+            <h1 className='text-6xl'>نوبت</h1>
+            <a href="#"><input type="search" className='border-0 rounded-lg w-11/12 h-6 ' placeholder='سرویس مورد نظر خود را جستجو کنید ...'/></a>
+            <div>
+
+            </div>
+        </header>
+    )
+}
