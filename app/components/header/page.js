b5d87a7e9a4bec3ee413a9e06b3eb68cbf8b21d1
Erfank <kahania726@gmail.com>
2023-06-20 08:58:43 +0330

    completed home

--- app/components/header/page.js
+++ app/components/header/page.js
@@ -3,13 +3,13 @@ import axios from "axios";
 
 export default function Header() {
     return (
-        <header className='bg-gray-950 h-3/5 text-center pt-14 pb-'>
+        <header className='bg-gray-950 h-4/6 md:h-4/6 lg:h-3/5 text-center pt-14'>
             <h1 className='text-6xl text-white'>نوبت</h1>
-            <a href="#"><input type="search" className='border border-amber-50 rounded-lg w-11/12 h-9 p-3 mt-16 bg-gray-950 placeholder:text-right placeholder:text-white' placeholder='سرویس مورد نظر خود را جستجو کنید'/></a>
+            <a href="#"><input type="search" className='search-border rounded-lg w-11/12 h-9 p-3 mt-16 bg-gray-950 font-size placeholder:text-right placeholder:text-gray-300' placeholder='سرویس مورد نظر خود را جستجو کنید...'/></a>
             <div className='mt-14 px-12'>
-                <div className='flex flex-col text-center'>
+                <div className='flex flex-col text-center w-24 items-center'>
                     <img src="" className='rounded-full bg-gray-700 h-24 w-24' alt="category"/>
-                    <h1 className='text-white'>مدل المانی</h1>
+                    <h1 className='text-white mt-2'>مدل المانی</h1>
                 </div>
             </div>
         </header>
--- app/page.js
+++ app/page.js
@@ -2,23 +2,45 @@ import './globals.css';
 import axios from "axios";
 import React from "react";
 import Header from "@/app/components/header/page";
+import HairStyle from "@/app/components/homeCard/hairStyle";
+import Suggestion from "@/app/components/homeCard/suggestion";
 
-export default async function Home() {
+export default async function Home(){
   return(
       <html>
         <head>
-          <meta charset="UTF-8"/>
-          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
-          <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-          <title>نوبت</title>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+            <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+            <title>نوبت</title>
         </head>
-        <body>
-          <Header />
+        <body dir='rtl'>
+            <Header />
+            <section className='py-6 px-3 sm:px-8'>
+                <div className='relative'>
+                    <h1 className='text-3xl font-bold'>پیشنهاد برای شما</h1>
+                    {/*<div className='absolute position'><img src="" alt="arrow"/></div>*/}
+                </div>
+                <HairStyle />
+            </section>
+            <section className='w-5/6 h-48 flex flex-col mx-auto px-4 py-3 bg-gray-200 rounded-2xl'>
+                <div className='relative'>
+                    <h1 className='text-xl md:text-2xl font-bold'>سرویس های پیشنهادی</h1>
+                    <div className='w-20 h-20 bg-white box-shadow rounded-full absolute position'></div>
+                </div>
+                <Suggestion />
+            </section>
+            <section className='py-6 px-3 sm:px-8 mt-5'>
+                <div className='relative'>
+                    <h1 className='text-3xl font-bold'>سرویس دهنده ها</h1>
+                    {/*<div className='absolute position'><img src="" alt="arrow"/></div>*/}
+                </div>
+                <HairStyle />
+            </section>
         </body>
       </html>
   )
 }
-
 
 async function getServiceSlide() {
   const {data} = await axios.get('http://127.0.0.1:8000/api/')
