87e7c5e5f031e5192dd14712b0cafe71c2ad08b3
Erfank <kahania726@gmail.com>
2023-07-10 17:28:38 +0330

    edit profile

--- app/profile/edit/page.js
+++ app/profile/edit/page.js
@@ -14,18 +14,18 @@ export default function EditProfile() {
             <body dir='rtl'>
                 <Back />
                 <section className='bg-gray-950 w-screen h-screen flex items-center justify-center'>
-                    <div className='w-72 md:w-1/2 bg-white text-center pt-6 rounded'>
+                    <div className='w-72 md:w-1/2 lg:h-96 bg-white text-center pt-6 rounded'>
                         <h1 className='text-2xl md:text-3xl'>ویرایش پروفایل</h1>
                         <form method='POST' className='w-full text-center'>
-                            <input type="text" name='first_name' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='نام'/>
+                            <input type="text" name='first_name' className='w-64 md:w-80 h-10 md:h-11 md:ml-1 mb-2 border-0 border-b' placeholder='نام'/>
 
-                            <input type="text" name='last_name' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='نام خانوادگی'/>
+                            <input type="text" name='last_name' className='w-64 md:w-80 h-10 md:h-11 md:me-1 mb-2 border-0 border-b' placeholder='نام خانوادگی'/>
 
-                            <input type="text" name='nationalcode' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='کدملی'/>
+                            <input type="text" name='nationalcode' className='w-64 md:w-80 h-10 md:h-11 md:me-1 md:ml-1 mb-2 border-0 border-b' placeholder='کدملی'/>
 
-                            <input type="text" name='phone' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='شماره همراه'/>
+                            <input type="text" name='phone' className='w-64 md:w-80 h-10 md:h-11  mb-2 border-0 border-b' placeholder='شماره همراه'/>
 
-                            <input type="text" name='city' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='شهر'/>
+                            <input type="text" name='city' className='w-64 md:w-80 h-10 md:h-11 mb-2 md:ml-1 border-0 border-b' placeholder='شهر'/>
 
                             <input type="text" name='address' className='w-64 md:w-80 h-10 md:h-11 mb-2 border-0 border-b' placeholder='ادرس'/>
 
