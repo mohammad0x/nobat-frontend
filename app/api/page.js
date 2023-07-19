8749acc122ede192fa00c33329d860f274565bbb
root <mohammadtaghdir7@gmail.com>
2023-07-19 18:05:28 +0330

    backend

--- app/api/page.js
+++ app/api/page.js
@@ -18,34 +18,15 @@ import React from "react";
 export default async function handler(req, res) {
     const data  = req
     const obj = data.searchParams
-    // console.log(obj)
     for (let y in obj){
-        // console.log( y)
         const jsObject = JSON.parse(y);
         console.log(jsObject.profile[1].id)
 
 
-        //     const modifiedString = y.split('[').join('');
-    //     const modifiedStrings = modifiedString.split(']').join('');
-    //     const modifiedStringss = modifiedStrings.split('{"profile":').join('');
-    //     let r = []
-    //     r.push(modifiedStringss)
-    //     console.log(r[1]);
-    //     for(let i of r){
-    //         console.log(typeof i)
+
         }
 
-        // const jsObject = JSON.parse(modifiedStrings);
 
-        // console.log(jsObject);
-
-
-        // console.log(jsonObject);
-        // console.log(modifiedStrings)
-
-
-
-    // }
 
     return (
 
