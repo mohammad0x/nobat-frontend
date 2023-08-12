9052e63515d4e20b8e0241edbb9f1b6f86bfb7e1
Erfank <kahania726@gmail.com>
2023-08-12 16:57:08 +0330

    merge

--- app/service/page.js
+++ app/service/page.js
@@ -1,7 +1,22 @@
 import React from "react";
 import Back from "@/app/components/backBtn/page";
 
-export default function NewService() {
+export default function NewService(req, res) {
+    const obj =  req.searchParams
+    let y = getDate(obj)
+
+    function getDate(obj){
+        for(let profile in obj ){
+            var jsObject =  JSON.parse(profile);
+        }
+        return jsObject
+    }
+
+    const list = []
+    for (let cate of y.category){
+        list.push([cate.id, cate.title])
+    }
+    console.log(list)
     return (
         <html lang="en">
         <head>
@@ -17,6 +32,14 @@ export default function NewService() {
                     <div className='w-72 md:w-1/2 w-98 bg-white pt-6 rounded text-center'>
                         <h1 className='text-2xl md:text-3xl mb-4'>سرویس جدید</h1>
                         <form method='POST' className='w-full h-full text-center flex flex-col items-center'>
+                            <select name="category" id="category" className='w-64 md:w-96 h-10 md:h-11 mb-2 border-0 border-b'>
+                                {list.map(item => {
+                                    return (
+                                        <option value={item[0]}>{item[1]}</option>
+                                    )
+                                })}
+                            </select>
+
                             <input type="text" name='title' className='w-64 md:w-96 h-10 md:h-11 mb-2 border-0 border-b' placeholder='title'/>
 
                             <input type="number" name='price' className='w-64 md:w-96 h-10 md:h-11 mb-2 border-0 border-b' placeholder='قیمت'/>
