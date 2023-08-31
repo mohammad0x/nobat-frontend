ec71d1f2e26967af92abeb8fa5fee58c63528381
Erfank <kahania726@gmail.com>
2023-08-31 20:00:22 +0330

    merge

--- app/login/page.js
+++ app/login/page.js
@@ -18,7 +18,7 @@ export default function Login() {
                             <form action="" method="post" className='flex flex-col mt-5 items-center'>
                                 <input type="email" name="email" id="email" className='w-64 border-b border-0 md:w-72 lg:w-80 h-8 lg:h-10 rounded mb-4 ' placeholder='ایمیل'/>
                                 <input type="password" name='password' className='w-64 border-b border-0 md:w-72 lg:w-80 h-8 lg:h-10 rounded mb-4 ' placeholder='رمزعبور'/>
-                                <Link href='../resetPassword/email/' className='text-sm text-right text-blue-600 hover:text-blue-800 duration-200'>فراموشی رمزعبور</Link>
+                                <Link href='../passwordPhone' className='text-sm text-right text-blue-600 hover:text-blue-800 duration-200'>فراموشی رمزعبور</Link>
                                 <input type="submit" value="ورود" className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-5'/>
                             </form>
                             <p className='text-sm mt-3'>اکانت ندارید؟ <a className='text-blue-500 hover:text-blue-800 duration-200' href="../register">ثبت نام</a> کنید</p>
--- app/resetPassword/newPassword/page.js
+++ app/resetPassword/newPassword/page.js
@@ -16,9 +16,9 @@ export default function Email() {
                     <div className='w-72 md:w-1/2 w-98 bg-white py-6 rounded text-center'>
                         <h1 className='text-2xl md:text-3xl mb-8'>رمزعبور جدید</h1>
                         <form method='POST' className='w-full h-full text-center flex flex-col items-center'>
-                            <input type="password" name='new_password' className='w-64 md:w-96 h-10 md:h-11 mb-5 border-0 border-b' placeholder='رمزعبور جدید'/>
+                            <input type="password" name='new_password1' className='w-64 md:w-96 h-10 md:h-11 mb-5 border-0 border-b' placeholder='رمزعبور جدید'/>
 
-                            <input type="password" name='new_password' className='w-64 md:w-96 h-10 md:h-11 mb-10 border-0 border-b' placeholder='تکرار رمزعبور'/>
+                            <input type="password" name='new_password2' className='w-64 md:w-96 h-10 md:h-11 mb-10 border-0 border-b' placeholder='تکرار رمزعبور'/>
 
                             <input type="submit" className='w-32 h-12 md:w-44 lg:h-14 rounded-full border border-gray-700 hover:border-0 hover:bg-gray-700 hover:text-white duration-300' value='تایید'/>
                         </form>
